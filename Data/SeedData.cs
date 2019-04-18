efa85e451a62c312c69a65315c0f2200c00ad82e
Dmitry Brezhnev <dubtar@gmail.com>
2019-04-18 09:20:21 +0300

    Инициализация базы и применение миграций при запуске.

---
+++ Data/SeedData.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace VeloTiming.Data
+{
+    internal class SeedData
+    {
+        internal static void Initialize(IServiceProvider services)
+        {
+            // Nothing to seed yet
+        }
+    }
+}
