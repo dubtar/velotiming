c41b5aea31a622d34f2d740e494c3a9d23f517f3
Dmitry Brezhnev <dubtar@gmail.com>
2019-10-07 10:21:01 +0300

    clean tree

--- VeloTiming.Tests/MainServiceTests.cs
+++ VeloTiming.Tests/MainServiceTests.cs
@@ -11,25 +11,31 @@ namespace VeloTiming.Tests
     public class MainServiceTests
     {
         MainService sut;
-        
+
 
         public MainServiceTests()
         {
-            var taskQueueMock = new Mock<IBackgroundTaskQueue>()
-                .Setup(t => t.QueueBackgroundWorkItem(It.IsAny<Func<System.Threading.CancellationToken, Task>>()))
-                .Callback<Func<System.Threading.CancellationToken, Task>>( action => { action(new System.Threading.CancellationToken()).Wait(); });
+            var taskQueueMock = new Mock<IBackgroundTaskQueue>();
 
-            sut = new MainService(Mock.Of<IHubContext<ResultHub, IResultHub>>(), Mock.Of<IBackgroundTaskQueue>());
+            taskQueueMock.Setup(t => t.QueueBackgroundWorkItem(It.IsAny<Func<System.Threading.CancellationToken, Task>>()))
+                .Callback<Func<System.Threading.CancellationToken, Task>>(action =>
+                {
+                    action(new System.Threading.CancellationToken()).Wait();
+                });
+
+            var iHubContextMock = new Mock<IHubContext<ResultHub, IResultHub>();
+
+            sut = new MainService(iHubContextMock.Object, taskQueueMock.Object);
         }
 
         [Fact]
         public void Test1()
         {
 
-            sut.AddNumberAndTime("1", new System.DateTime(2019,1,1,1,1,1), "rfid");
+            sut.AddNumberAndTime("1", new System.DateTime(2019, 1, 1, 1, 1, 1), "rfid");
 
             var results = sut.GetMarks().ToArray();
-            Assert.Equal(1, results.Length);
+            Assert.Single(results);
             Assert.Equal("1", results[0].Number);
             Assert.Equal(1, results[0].Place);
             Assert.Equal(1, results[0].Lap);
