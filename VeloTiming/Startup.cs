392b829cb600471595ffcf124588225eb8ccc9eb
Dmitry Brezhnev <dubtar@gmail.com>
2020-07-20 21:39:00 +0300

    turn off error

--- VeloTiming/Startup.cs
+++ VeloTiming/Startup.cs
@@ -82,7 +82,7 @@ namespace VeloTiming
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                // app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
